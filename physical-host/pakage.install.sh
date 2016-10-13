28bedaa1be50e27b61130d136a1f46a7f4dd0e33
tsuyukusa <tsuyukusa@users.noreply.github.com>
2016-10-14 03:33:38 +0900

    update

--- physical-host/pakage.install.sh
+++ physical-host/pakage.install.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
-# common
-sudo apt update
+export USERNAME=tm
+export INBRIDGE=ovs-lan1
+export INNIC0=enp0s25
+export INNIC1=eth2
+export WLNIC=wls3
 
-# docker
-wget https://get.docker.com/ | sudo sh
+function install-package(){
+    # common
+    sudo apt update
 
-# ovs
-sudo apt install openvswitch-switch openvswitch-common
+    # docker
+    wget https://get.docker.com/ | sudo sh
+    sudo usermod -aG docker $USERNAME
 
-# ovs-docker
-wget https://raw.githubusercontent.com/openvswitch/ovs/master/utilities/ovs-docker
-chmod +x ovs-docker
-sudo mv ovs-docker /usr/local/bin/
+    # ovs
+    sudo apt install openvswitch-switch openvswitch-common -y
 
-sudo ovs-vsctl add-br ovs-lan1
+    # ovs-docker
+    wget https://raw.githubusercontent.com/openvswitch/ovs/master/utilities/ovs-docker
+    chmod +x ovs-docker
+    sudo mv ovs-docker /usr/local/bin/
+}
 
+function setup-ovs(){
+    sudo ovs-vsctl add-br $INBRIDGE
+    sudo ovs-vsctl add-port $INNIC1
+}
+
+install-package
+setup-ovs
